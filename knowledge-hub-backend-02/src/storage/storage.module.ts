2046ac358bf08426bf3e90a524263c7be8abce12
guang <18365641385@163.com>
2026-08-07 18:58:10 +0800

    知识库项目代码迁移

--- knowledge-hub-backend-02/src/storage/storage.module.ts
+++
@@ -1,9 +0,0 @@
-import { Global, Module } from '@nestjs/common';
-import { RustfsService } from './rustfs.service';
-
-@Global()
-@Module({
-  providers: [RustfsService],
-  exports: [RustfsService],
-})
-export class StorageModule {}
